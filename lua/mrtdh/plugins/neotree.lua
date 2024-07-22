f4c3c6a8d6e0d99f0ad6f3c0705a1ef3bdd697aa
Muhammad Murtadho <muhammad.murtadho@btpn.com>
2024-07-22 16:29:14 +0700

    make neotree follow current buffer

--- lua/mrtdh/plugins/neotree.lua
+++ lua/mrtdh/plugins/neotree.lua
@@ -7,4 +7,13 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+		})
+	end,
 }
